11b4b10621bd2122f50429d9e3689921a81b7316
Nikita <nikitasa1997@gmail.com>
2021-01-12 22:51:01 +0300

    myapp.py

---
+++ examples/myapp.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+
+def app(environ, start_response):
+    data = bytes('Местное время: ' + datetime.now(), 'UTF-8')
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(data)))
+    ])
+    return iter([data])

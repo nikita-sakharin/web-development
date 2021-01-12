b491f2a0a9032463fc6d32c9fdeea8e1dac3fa0c
Nikita <nikitasa1997@gmail.com>
2021-01-12 22:56:30 +0300

    myapp.py

--- examples/myapp.py
+++ examples/myapp.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 
 def app(environ, start_response):
-    data = bytes('Местное время: ' + datetime.now(), 'UTF-8')
+    data = bytes('Местное время: ' + str(datetime.now()), 'UTF-8')
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))

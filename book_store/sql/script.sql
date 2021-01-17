b0c5f0d11b0b6fe0e543e41801470434a51f04e4
Nikita <nikitasa1997@gmail.com>
2021-01-17 16:07:13 +0300

    sql

--- book_store/sql/script.sql
+++ book_store/sql/script.sql
@@ -1,6 +1,7 @@
 DROP DATABASE IF EXISTS book_store_database;
 DROP USER IF EXISTS book_store_user;
 
+--Just for example. Don't save password in git.
 CREATE USER book_store_user PASSWORD 'IX#xZ&5kojzu!*hQyVRo';
 CREATE DATABASE book_store_database WITH
     OWNER book_store_user

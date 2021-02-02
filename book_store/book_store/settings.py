e8c46f2cfc39d1f3809522e3e8397140150e8db5
Nikita <nikitasa1997@gmail.com>
2021-02-02 22:37:21 +0300

    db

--- book_store/book_store/settings.py
+++ book_store/book_store/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'book_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'book_store_database',
+        'USER': 'book_store_user',
+        'PASSWORD': 'IX#xZ&5kojzu!*hQyVRo',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 

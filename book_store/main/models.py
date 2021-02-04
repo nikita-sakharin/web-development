9e313b13a6cfda37a052e76659ff33948e78a6a1
Nikita <nikitasa1997@gmail.com>
2021-02-04 14:00:58 +0300

    next

--- book_store/main/models.py
+++ book_store/main/models.py
@@ -59,3 +59,59 @@ class Book(Model):
                 name='book_isbn_check'),
             CheckConstraint(check=Q(price__gt=0), name='book_price_check'),
         ]
+"""
+from main.models import Author, Book, Genre
+
+Author(full_name='Александр Сергеевич Пушкин', birth_date='1799-06-06',
+    death_date='1837-02-10').save()
+Author(full_name='Григорий Михайлович Фихтенгольц', birth_date='1888-06-05',
+    death_date='1959-06-26').save()
+Author(full_name='Лев Николаевич Толстой', birth_date='1828-09-09',
+    death_date='1910-11-20').save()
+Author(full_name='Фёдор Михайлович Достоевский', birth_date='1821-11-11',
+    death_date='1881-02-09').save()
+
+Author(full_name='Клиффорд Штайн', birth_date='1965-12-14').save()
+Author(full_name='Рональд Линн Ривест', birth_date='1947-01-01').save()
+Author(full_name='Томас Х. Кормен', birth_date='1956-01-01').save()
+Author(full_name='Чарльз Эрик Лейзерсон', birth_date='1953-11-10').save()
+
+Genre(name='Драма').save()
+Genre(name='Компьютерные науки').save()
+Genre(name='Математика').save()
+Genre(name='Роман').save()
+
+Book(title='Алгоритмы: построение и анализ', pub_year='1990-01-01',
+    isbn='9785845920164', price=5500).save()
+Book(title='Анна Каренина', pub_year='1875-01-01',
+    isbn='9785170878888', price=222).save()
+Book(title='Борис Годунов', pub_year='1831-01-01',
+    isbn='9785040978076', price=136).save()
+Book(title='Война и мир', pub_year='1865-01-01',
+    isbn='9785389071230', price=297).save()
+Book(title='Евгений Онегин', pub_year='1825-01-01',
+    isbn='9785170931217', price=190).save()
+Book(title='Курс дифференциального и интегрального исчисления',
+    pub_year='1947-01-01', isbn='9785811470617', price=2995).save()
+Book(title='Преступление и наказание', pub_year='1866-01-01',
+    isbn='9785170906307', price=176).save()
+
+Book.objects.get(id=1).author.add(5)
+Book.objects.get(id=1).author.add(6)
+Book.objects.get(id=1).author.add(7)
+Book.objects.get(id=1).author.add(8)
+Book.objects.get(id=2).author.add(3)
+Book.objects.get(id=3).author.add(1)
+Book.objects.get(id=4).author.add(3)
+Book.objects.get(id=5).author.add(1)
+Book.objects.get(id=6).author.add(2)
+Book.objects.get(id=7).author.add(4)
+
+Book.objects.get(id=1).genre.add(2)
+Book.objects.get(id=2).genre.add(4)
+Book.objects.get(id=3).genre.add(1)
+Book.objects.get(id=4).genre.add(4)
+Book.objects.get(id=5).genre.add(4)
+Book.objects.get(id=6).genre.add(3)
+Book.objects.get(id=7).genre.add(4)
+"""

170b7dabf2678d0e0c04ab1679cc91af49e2aa7c
Nikita <nikitasa1997@gmail.com>
2021-02-04 18:59:25 +0300

    admin

--- book_store/main/admin.py
+++ book_store/main/admin.py
@@ -5,20 +5,20 @@ from main.models import Author, Book, Genre
 class AuthorAdmin(ModelAdmin):
     list_display = ('id', 'full_name', 'birth_date')
 
-admin.site.register(Author, AuthorAdmin)
+site.register(Author, AuthorAdmin)
 
 class BookAdmin(ModelAdmin):
     list_display = ('id', 'title', 'pub_year')
 
-admin.site.register(Book, BookAdmin)
+site.register(Book, BookAdmin)
 
 class GenreAdmin(ModelAdmin):
     list_display = ('id', 'name')
 
-admin.site.register(Genre, GenreAdmin)
+site.register(Genre, GenreAdmin)
 
 class UserCourseAdmin(ModelAdmin):
     list_display = ('id', 'course_id_id', 'user_id_id')
     pass
 
-admin.site.register(UserCourse, UserCourseAdmin)
+site.register(UserCourse, UserCourseAdmin)

cf4c1c832bad6ceee7a565d9f942d98c067d3f6b
Nikita <nikitasa1997@gmail.com>
2021-02-04 19:15:42 +0300

    admin

--- book_store/main/admin.py
+++ book_store/main/admin.py
@@ -4,16 +4,19 @@ from main.models import Author, Book, Genre
 
 class AuthorAdmin(ModelAdmin):
     list_display = ('id', 'full_name', 'birth_date')
+    list_filter = ('full_name', 'birth_date', 'death_date')
 
 site.register(Author, AuthorAdmin)
 
 class BookAdmin(ModelAdmin):
     list_display = ('id', 'title', 'pub_year')
+    list_filter = ('title', 'pub_year', 'isbn', 'price')
 
 site.register(Book, BookAdmin)
 
 class GenreAdmin(ModelAdmin):
     list_display = ('id', 'name')
+    list_filter = ('name',)
 
 site.register(Genre, GenreAdmin)
 """

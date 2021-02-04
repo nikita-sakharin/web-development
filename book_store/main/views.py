df9e5e84ab0cb59ad8f0907456079417cfc059b8
Nikita <nikitasa1997@gmail.com>
2021-02-04 15:25:51 +0300

    lab 5 init

--- book_store/main/views.py
+++ book_store/main/views.py
@@ -1,33 +1,11 @@
 from django.shortcuts import render
 
 def book_detail(request, book_id):
-    '''
     try:
         book = Book.objects.get(id=book_id)
     except Book.DoesNotExist:
         raise Http404("There is no such book unfortunately")
     return render(request, 'book.html', {'book': book})
-    '''
-    return render(request, 'book.html', {'book': {
-        'title': 'Война и Мир',
-        'author': 'Лев николаевич Толстой',
-        'year': 1867,
-        'price': 3.99
-    }})
 
 def books(request):
-    # return render(request, 'books.html', {'books': Book.objects.all()})
-    return render(request, 'books.html', {'books': [
-        {
-            'title': 'Война и Мир',
-            'author': 'Лев николаевич Толстой',
-            'year': 1867,
-            'price': 3.99
-        },
-        {
-            'title': 'Introduction to Algorithms',
-            'author': 'Thomas H. Cormen',
-            'year': 2009,
-            'price': 49.99
-        }
-    ]})
+    return render(request, 'books.html', {'books': Book.objects.all()})

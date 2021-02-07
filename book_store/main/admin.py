8db8fb0896a92188e8f912762085fe02cf378df8
Nikita <nikitasa1997@gmail.com>
2021-02-07 20:58:58 +0300

    next

--- book_store/main/admin.py
+++ book_store/main/admin.py
@@ -2,29 +2,23 @@ from django.contrib.admin import ModelAdmin, TabularInline, site
 
 from main.models import Author, Book, Genre
 
-class BookAuthorsInline(TabularInline):
-    model = Book.authors.through
-
 class AuthorAdmin(ModelAdmin):
     list_display = ('id', 'full_name', 'birth_date')
     list_filter = ('full_name', 'birth_date', 'death_date')
-    inlines = [
-        BookAuthorsInline,
-    ]
 
 site.register(Author, AuthorAdmin)
-
-class BookGenresInline(TabularInline):
-    model = Book.genres.through
 
 class GenreAdmin(ModelAdmin):
     list_display = ('id', 'name')
     list_filter = ('name',)
-    inlines = [
-        BookGenresInline,
-    ]
 
 site.register(Genre, GenreAdmin)
+
+class BookAuthorsInline(TabularInline):
+    model = Book.authors.through
+
+class BookGenresInline(TabularInline):
+    model = Book.genres.through
 
 class BookAdmin(ModelAdmin):
     list_display = ('id', 'title', 'pub_year')

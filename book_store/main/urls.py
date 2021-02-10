ef22fa9109ce8e29b89448ced8b45e23b55c5fcd
Nikita <nikitasa1997@gmail.com>
2021-02-10 23:20:13 +0300

    next

--- book_store/main/urls.py
+++ book_store/main/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('book/<int:book_id>', views.book_detail, name='book_detail'),
+    path('books/<int:book_id>', views.book_detail, name='book_detail'),
     path('books', views.books, name='books'),
 ]
 # TODO декоратор

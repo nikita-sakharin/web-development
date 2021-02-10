c21739dd4b93942c267b32d901e825de47167ed9
Nikita <nikitasa1997@gmail.com>
2021-02-10 23:21:34 +0300

    next

--- book_store/main/views.py
+++ book_store/main/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 
-from main.models import Book
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from main.models import Author, Book, Genre
+from main.serializers import AuthorSerializer, BookSerializer, GenreSerializer
+
+class BookView(APIView):
+    def get(self, request, format=None):
+        usernames = [user.username for user in User.objects.all()]
+        return Response(usernames)
+
+    def post(self, request):
+        form = BookForm(request.GET) # GET or POST ?
+        if form.is_valid():
+            course = form.save()
+            return Response({'post_id': course.id})
+        return Response({'error': form.errors})
 
 def book_detail(request, book_id):
     try:
@@ -11,3 +27,22 @@ def book_detail(request, book_id):
 
 def books(request):
     return render(request, 'books.html', {'books': Book.objects.all()})
+"""
+from django.shortcuts import render
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Movie
+from .serializers import MovieSerializer
+
+class MovieView(APIView):
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response({'movies': serializer.data})
+
+    def post(self, request):
+        # ...
+        return Response({'error': None, 'result': 'Фильм успешно добавлен'})
+"""

3bc16b853c7165fef902c5e5368c830be19831b6
Nikita <nikitasa1997@gmail.com>
2021-02-13 18:17:40 +0300

    next

--- book_store/main/serializers.py
+++ book_store/main/serializers.py
@@ -1,24 +1,23 @@
 from rest_framework.serializers import (CharField, DateField, DecimalField,
-    Serializer)
+    ModelSerializer, Serializer)
 
 class AuthorSerializer(Serializer):
     full_name = CharField(max_length=255)
     birth_date = DateField()
     death_date = DateField()
 
-"""
-from rest_framework.serializers import ModelSerializer
 class BookSerializer(ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
-"""
+        exclude = ['id']
 
+"""
 class BookSerializer(Serializer):
     title = CharField(max_length=255)
     pub_year = DateField()
     isbn = CharField(max_length=13)
     price = DecimalField(max_digits=19, decimal_places=2)
-
+"""
 class GenreSerializer(Serializer):
     name = CharField(max_length=255)

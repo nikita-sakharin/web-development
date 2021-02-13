1b8932cd8f8b4fc7d6b5f3f3b64d570b90d0f5fc
Nikita <nikitasa1997@gmail.com>
2021-02-13 18:19:05 +0300

    next

--- book_store/main/serializers.py
+++ book_store/main/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework.serializers import (CharField, DateField, DecimalField,
     ModelSerializer, Serializer)
 
+from main.models import Book
+
 class AuthorSerializer(Serializer):
     full_name = CharField(max_length=255)
     birth_date = DateField()

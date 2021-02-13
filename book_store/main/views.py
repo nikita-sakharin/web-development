bf4406563143ed14de02ab5d3de81dfdd376ce80
Nikita <nikitasa1997@gmail.com>
2021-02-13 20:08:49 +0300

    next

--- book_store/main/views.py
+++ book_store/main/views.py
@@ -1,27 +1,18 @@
 from django.shortcuts import render
 
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 
 from main.models import Author, Book, Genre
 from main.serializers import AuthorSerializer, BookSerializer, GenreSerializer
 
-class BookView(APIView):
-    serializer_class = BookSerializer
-    def get(self, request, format=None):
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        return Response(serializer.data)
-    """
-    def post(self, request):
-        form = BookForm(request.GET) # GET or POST ?
-        if form.is_valid():
-            course = form.save()
-            return Response({'post_id': course.id})
-        return Response({'error': form.errors})
-    """
-    def post(self, request):
-        return Response({'error': None, 'result': 'Книга успешно добавлена'})
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
 def book_detail(request, book_id):
     try:
@@ -40,6 +31,23 @@ from rest_framework.response import Response
 
 from .models import Movie
 from .serializers import MovieSerializer
+
+class BookView(APIView):
+    serializer_class = BookSerializer
+    def get(self, request, format=None):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        form = BookForm(request.GET) # GET or POST ?
+        if form.is_valid():
+            course = form.save()
+            return Response({'post_id': course.id})
+        return Response({'error': form.errors})
+
+    def post(self, request):
+        return Response({'error': None, 'result': 'Книга успешно добавлена'})
 
 class MovieView(APIView):
     def get(self, request):

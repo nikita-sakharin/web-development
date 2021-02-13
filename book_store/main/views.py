67c95ce2147d999bbec83d3a3b015910e63685eb
Nikita <nikitasa1997@gmail.com>
2021-02-13 20:32:00 +0300

    next

--- book_store/main/views.py
+++ book_store/main/views.py
@@ -1,18 +1,34 @@
 from django.shortcuts import render
 
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework.generics import (ListCreateAPIView, RetrieveUpdateAPIView)
 
 from main.models import Author, Book, Genre
 from main.serializers import AuthorSerializer, BookSerializer, GenreSerializer
 
-class SnippetList(generics.ListCreateAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+class AuthorDetail(RetrieveUpdateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+class AuthorList(ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class BookDetail(RetrieveUpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookList(ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class GenreDetail(RetrieveUpdateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+class GenreList(ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
 
 def book_detail(request, book_id):
     try:
@@ -23,39 +39,3 @@ def book_detail(request, book_id):
 
 def books(request):
     return render(request, 'books.html', {'books': Book.objects.all()})
-"""
-from django.shortcuts import render
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-from .models import Movie
-from .serializers import MovieSerializer
-
-class BookView(APIView):
-    serializer_class = BookSerializer
-    def get(self, request, format=None):
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        form = BookForm(request.GET) # GET or POST ?
-        if form.is_valid():
-            course = form.save()
-            return Response({'post_id': course.id})
-        return Response({'error': form.errors})
-
-    def post(self, request):
-        return Response({'error': None, 'result': 'Книга успешно добавлена'})
-
-class MovieView(APIView):
-    def get(self, request):
-        movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True)
-        return Response({'movies': serializer.data})
-
-    def post(self, request):
-        # ...
-        return Response({'error': None, 'result': 'Фильм успешно добавлен'})
-"""

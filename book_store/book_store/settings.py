88a0af0594b01f9a276cf249572f743b5b23d0b9
Nikita <nikitasa1997@gmail.com>
2021-02-15 21:08:25 +0300

    next

--- book_store/book_store/settings.py
+++ book_store/book_store/settings.py
@@ -132,3 +132,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# AWS_ACCESS_KEY_ID =
+# AWS_SECRET_ACCESS_KEY =
+# AWS_STORAGE_BUCKET_NAME =
+# AWS_S3_ENDPOINT_URL =

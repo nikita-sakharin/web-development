e5d66fdc992113abefc23fee4af52fa7377edf26
Nikita <nikitasa1997@gmail.com>
2021-02-20 23:03:13 +0300

    next

--- book_store/book_store/urls.py
+++ book_store/book_store/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'),
         name='logout'),
+    # path('media/<int:user_id>/<filename>', ),
     path('social/', include('social_django.urls', namespace='social')), # name ?
 ]

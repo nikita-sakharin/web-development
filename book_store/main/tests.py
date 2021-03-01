0613c86b3b204e5709cf0c79daf842f27c2544e8
Nikita <nikitasa1997@gmail.com>
2021-03-01 23:06:54 +0300

    next

--- book_store/main/tests.py
+++ book_store/main/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from locale import getdefaultlocale
 
-# Create your tests here.
+from django.test import Client, TestCase
+
+from factory import Factory, Faker, SubFactory
+
+from main.models import Author, Book, Genre
+from main.serializers import AuthorSerializer, BookSerializer, GenreSerializer
+
+class AuthorFaker(Factory):
+    class Meta:
+        model = Author
+    name = Faker('word', locale=getdefaultlocale()[0])
+
+class GenreFaker(Factory):
+    class Meta:
+        model = Genre
+    name = Faker('word', locale=getdefaultlocale()[0])
+
+class BookFaker(Factory):
+    class Meta:
+        model = Movie
+
+    title = Faker('sentence', nb_words=3, locale=getdefaultlocale()[0])
+    year = Faker('year')
+    genre = SubFactory(GenreFaker)
+
+class MovieAPITest(TestCase):
+    MAX_MOVIES_COUNT = 20
+
+    def setUp(self):
+        self.client = Client()
+        self.movies = MovieFaker.create_batch(self.MAX_MOVIES_COUNT)
+        for movie in self.movies:
+            print(movie.title, movie.year, movie.genre)
+            movie.genre.save()
+            movie.save()
+
+    def test_movies_get(self):
+        response = self.client.get('/api/movies/')
+        self.assertEqual(Movie.objects.count(), self.MAX_MOVIES_COUNT)
+        self.assertEqual(response.status_code, 200)
+        expected = {'movies': MovieSerializer(self.movies, many=True).data}
+        self.assertEqual(response.json(), expected)
+
+    def tearDown(self):
+        pass

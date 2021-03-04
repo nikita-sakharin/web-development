42387b56fe3213ac7188c8d0607811ec7f71ac62
nikita <nikitasa1997@gmail.com>
2021-03-04 23:06:41 +0300

    test

--- book_store/main/tests.py
+++ book_store/main/tests.py
@@ -2,10 +2,13 @@ from locale import getdefaultlocale
 
 from django.test import Client, TestCase
 
-from factory import Factory, Faker, SubFactory, post_generation
+from factory import Factory, Faker, Sequence, SubFactory, post_generation
+import faker
 
 from main.models import Author, Book, Genre
 from main.serializers import AuthorSerializer, BookSerializer, GenreSerializer
+
+UNIQUE_FAKER = faker.Faker(locale=getdefaultlocale()[0]).unique 
 
 class AuthorFaker(Factory):
     class Meta:
@@ -16,21 +19,22 @@ class AuthorFaker(Factory):
 class GenreFaker(Factory):
     class Meta:
         model = Genre
-    name = Faker('word', locale=getdefaultlocale()[0])
+    name = Sequence(lambda _: UNIQUE_FAKER.word())
 
 class BookFaker(Factory):
     class Meta:
         model = Book
 
     title = Faker('sentence', nb_words=3, locale=getdefaultlocale()[0])
-    pub_year = Faker('date_object')
-    isbn = Faker('isbn13', separator='')
-    price = Faker('pydecimal', positive=True)
+    pub_year = Faker('date', pattern='%Y-01-01')
+    isbn = Sequence(lambda _: UNIQUE_FAKER.isbn13(separator=''))
+    price = Faker('pydecimal', positive=True, min_value=1)
 
     @post_generation
     def authors(self, create, extracted, **kwargs):
         if not create or not extracted:
             return
+        self.save()
         for author in extracted:
             self.authors.add(author)
 
@@ -38,6 +42,7 @@ class BookFaker(Factory):
     def genres(self, create, extracted, **kwargs):
         if not create or not extracted:
             return
+        self.save()
         for genre in extracted:
             self.genres.add(genre)
 
@@ -47,20 +52,24 @@ class BookAPITest(TestCase):
         self.books = []
         for _ in range(100):
             author = AuthorFaker.create()
+            author.save()
             genre = GenreFaker.create()
+            genre.save()
             book = BookFaker.create(authors=[author], genres=[genre])
-            book.pub_year = book.pub_year.replace(day=1, month=1)
             self.books.append(book)
-            # book.authors.save()
-            # book.genres.save()
-            book.save()
 
-    def test_books_get(self):
+    def test_book_detail(self):
+        for book in self.books:
+            response = self.client.get(F'/api/books/{book.id}/')
+            self.assertEqual(response.status_code, 200)
+            data = BookSerializer(book).data
+            self.assertCountEqual(response.json(), data)
+
+    def test_book_list(self):
         response = self.client.get('/api/books/')
-        self.assertEqual(Book.objects.count(), len(self.books))
         self.assertEqual(response.status_code, 200)
-        expected = BookSerializer(self.books, many=True).data
-        self.assertEqual(response.json(), expected)
+        data = BookSerializer(self.books, many=True).data
+        self.assertCountEqual(response.json(), data)
 
     def tearDown(self):
         pass
